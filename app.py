df61ce5b4eb59d06347e6e9d399439444036ee27
blg <bailiguang2009@163.com>
2020-09-01 17:26:27 +0800

    第二章

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
-from flask import Flask, request, make_response, render_template
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-    # response = make_response('<h1>This document carries a cookie!</h1>')
-    # response.set_cookie('answer', '42')
-    # return response
 
-@app.route('/hello')
-def hello_world():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is {}</p>'.format(user_agent)
+@app.route('/test')
+def test_index():
+    return render_template('test.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
-# if __name__ == '__main__':
-#     app.run()
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
