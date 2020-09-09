f839bd30f43f5a53986966962b472f1c855064a9
blg <bailiguang2009@163.com>
2020-09-09 17:06:55 +0800

    大型结构，工厂函数，蓝图等1

--- hello/main/views.py
+++ hello/main/views.py
@@ -4,13 +4,20 @@ from . import main
 from .forms import NameForm
 
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', current_time=datetime.utcnow())
+
+@main.route('/test')
+def test_index():
+    return render_template('test.html', current_time=datetime.utcnow())
+
+# def index():
+#     form = NameForm()
+#     if form.validate_on_submit():
+#         old_name = session.get('name')
+#         if old_name is not None and old_name != form.name.data:
+#             flash('Looks like you have changed your name!')
+#         session['name'] = form.name.data
+#         return redirect(url_for('index'))
+#     return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
