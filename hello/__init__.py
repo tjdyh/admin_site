cfac58e160d4313880ff29ba5fa2b559d12c7bb5
blg <bailiguang2009@163.com>
2020-09-19 17:36:28 +0800

    更新

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 from flask_moment import Moment
 from config import config
+from flask_sqlalchemy import SQLAlchemy
+
 
 bootstrap = Bootstrap()
+mail = Mail()
 moment = Moment()
+db = SQLAlchemy()
 
 
 def create_app(config_name):
@@ -13,7 +18,9 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
+    mail.init_app(app)
     moment.init_app(app)
+    db.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
