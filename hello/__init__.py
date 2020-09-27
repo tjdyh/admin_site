730b498607e764aa3b2a7c40fb397e1d88120e9c
blg <bailiguang2009@163.com>
2020-09-27 17:21:57 +0800

    更新

--- hello/__init__.py
+++ hello/__init__.py
@@ -4,13 +4,15 @@ from flask_mail import Mail
 from flask_moment import Moment
 from config import config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
-
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -21,9 +23,12 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
 
     return app
--- hello/models.py
+++ hello/models.py
@@ -1,6 +1,12 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from . import db
+from . import login_manager
 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -11,7 +17,7 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
