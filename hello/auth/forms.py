d55ee750a6b4da2d6d1ad1c0beb3d165879756a3
blg <bailiguang2009@163.com>
2020-10-12 17:23:28 +0800

    更新10.3

--- hello/auth/forms.py
+++ hello/auth/forms.py
@@ -10,6 +10,7 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
 
+
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1,64), Email()])
     username = StringField('Username', validators=[DataRequired(), Length(1,64),Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
